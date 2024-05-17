40dc7671e1c0e490a0d8c7aba46052018fa32a1a
Tervel <danijeliazkov@yahoo.bg>
2024-05-17 19:24:50 +0300

    for loop exercises

---
+++ ForLoop_Lab/SumNumbers.java
@@ -0,0 +1,18 @@
+package ForLoop_Lab;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numbersCount = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i=0;i<numbersCount;i++) {
+            sum += Integer.parseInt(scanner.nextLine());
+        }
+
+        System.out.println(sum);
+    }
+}
